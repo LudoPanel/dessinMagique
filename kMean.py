c94d723179e956c0c35e16d837747f0ee0731913
Unknown <ludovic.panel@gmail.com>
2018-04-18 22:39:58 +0200

    Refacto

--- kMean.py
+++ kMean.py
@@ -26,7 +26,6 @@ def recupContour(imgInitiale, imgNettoyee):
 
     # On applique un gradient avec un gamma8
     structElement = strel.build('diamant', 1, 0)
-
     imageNiveauGris = cv2.cvtColor(imgNettoyee, cv2.COLOR_BGR2GRAY)
     imgAvecGradient = utils.gradient(imageNiveauGris, structElement)
 
@@ -36,26 +35,65 @@ def recupContour(imgInitiale, imgNettoyee):
 
     # On reconstitue une image couleur a partir de l'image en niveau de gris
     imgContour = imgInitiale
-
     index = imgSeuilee[:] == 255
     imgContour[:] = (0, 0, 0)
     imgContour[index] = (255, 255, 255)
 
+    elementOuverture = strel.build('disque', 1, None)
+    elementReconstruction = strel.build('carre', 1, None)
+
+    imgContour = utils.ouvertureReconstruction(imgContour, elementOuverture, elementReconstruction)
+    imgContour = utils.fermetureReconstruction(imgContour, elementOuverture, elementReconstruction)
+
     return imgContour
 
 
-# def nettoyageImage(image, pixelMax, largeurMax):
-#
-#     # Threshold it so it becomes binary
-#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     ret, thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)
-#
-#     # You need to choose 4 or 8 for connectivity type
-#     connectivity = 4
-#
-#     # Perform the operation
-#     output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
-#
-#
-#
-#     return imageNettoye
+def nettoyageImage(image, aireZoneMax, largeurMax):
+
+    # On transforme l'image en niveau de gris
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # On transforme l'image en binaire en appliquant un threshold
+    ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+
+    # On recupere les composantes connexes de notre image (technique de labelisation)
+    output = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
+
+    # On recupere les labels de nos zones
+    labels = output[1]
+
+    # On recupere les statistiques associees a chaque zone
+    stats = output[2]
+
+    imgNettoyee = image
+    taille = image.shape
+
+    # On itere sur nos pixels
+    # Pour chaque pixel, on recupere la zone associee afin de connaitre l'aire et la largeur de la bounding box
+    # Si c'est trop petit, alors on remplace la couleur par une couleur precedente
+    for x in range(0, taille[0]):
+        for y in range(0, taille[1]):
+            zone = labels[x][y]
+            if stats[zone, cv2.CC_STAT_AREA] < aireZoneMax or stats[zone, cv2.cv2.CC_STAT_WIDTH] < largeurMax:
+                imgNettoyee[x, y] = imgNettoyee[x - 2, y]
+
+    return imgNettoyee
+
+
+def ajouterIndicationCouleursZone(imgContour, imgNettoyee, rayonDisqueCouleur):
+
+    imgray = cv2.cvtColor(imgContour, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+
+    output = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
+    centroids = output[3]
+
+    structElement = strel.build_as_list('disque', rayonDisqueCouleur, None)
+
+    taille = imgContour.shape
+    for centroid in centroids[0:output[0]]:
+        for (i, j) in structElement:
+            if int(centroid[1]) < (taille[1] - 2) and int(centroid[0]) < (taille[0] - 2):
+                imgContour[i + int(centroid[1]), j + int(centroid[0])] = imgNettoyee[int(centroid[1]), int(centroid[0])]
+
+    return imgContour
--- main.py
+++ main.py
@@ -1,76 +1,34 @@
-import cv2, kMean, utils, strel
+import cv2, kMean, utils
 
-img = cv2.imread('Images/caillou.png')
+#######
+## Parametres a saisir par l'utilisateur
+#######
+
+nomImage = 'caillou.png'
+nombreDeCouleur = 4
+aireZoneMax = 400
+largeurZoneMax = 30
+rayonDisqueCouleur = 4
+
+#######
+## Fin parametre a saisir par l'utilisateur
+#######
+
+img = cv2.imread('Images/' + nomImage)
 utils.displayImage(img, 'Image initiale')
 
-# On applique kmean
-imgKMean = kMean.reducColor(img, 6)
+# On applique kmean pour reduire le nombre de couleurs de l'image
+imgKMean = kMean.reducColor(img, nombreDeCouleur)
 utils.displayImage(imgKMean, 'Image kmean')
 
 # On nettoie le resultat obtenu (on enleve les zones trop petites)
-imgray = cv2.cvtColor(imgKMean, cv2.COLOR_BGR2GRAY)
-
-# imgray = cv2.medianBlur(imgray,5)
-ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
-
-output = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
-
-# Get the results
-# The first cell is the number of labels
-num_labels = output[0]
-print num_labels
-# The second cell is the label matrix
-labels = output[1]
-# The third cell is the stat matrix
-stats = output[2]
-# The fourth cell is the centroid matrix
-centroids = output[3]
-
-imgNettoyee = imgKMean
-
-elementOuverture = strel.build('carre', 2, None)
-elementReconstruction = strel.build('carre', 2, None)
-#
-# imgNettoyee = utils.fermetureReconstruction(imgNettoyee, elementOuverture, elementReconstruction)
-# imgNettoyee = utils.ouvertureReconstruction(imgNettoyee, elementOuverture, elementReconstruction)
-
-taille = imgKMean.shape
-for x in range(0, taille[0]):
-    for y in range(0, taille[1]):
-        if stats[labels[x][y], cv2.CC_STAT_AREA] < 800 or stats[labels[x][y], cv2.cv2.CC_STAT_WIDTH] < 30:
-            imgNettoyee[x, y] = imgNettoyee[x-1, y]
-
-
-# imgNettoyee = kMean.nettoyageImage(img, pixelMax, largeurMax)
+imgNettoyee = kMean.nettoyageImage(imgKMean, aireZoneMax, largeurZoneMax)
 utils.displayImage(imgNettoyee, 'Image nettoyee')
 
 # On extrait les contours de l'image
 imgContour = kMean.recupContour(img, imgNettoyee)
-
-# imgray = cv2.cvtColor(imgKMean, cv2.COLOR_BGR2GRAY)
-# th3 = cv2.adaptiveThreshold(imgray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-# im2, contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#
-# imgContour = img
-# imgContour[:] = (255, 255, 255)
-#
-# cv2.drawContours(imgContour, contours, -1, (0, 0, 0), 2)
-
 utils.displayImage(imgContour, 'Image contour')
 
 # On pose les indications dans les zones
-imgray = cv2.cvtColor(imgContour, cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
-
-output = cv2.connectedComponentsWithStats(thresh, 4, cv2.CV_32S)
-centroids = output[3]
-
-structElement = strel.build_as_list('disque', 2, None)
-
-for centroid in centroids[0:output[0]]:
-    for (i, j) in structElement:
-        if int(centroid[1]) < (taille[1] - 2) and int(centroid[0]) < (taille[0] - 2):
-            imgContour[i + int(centroid[1]), j + int(centroid[0])] = imgNettoyee[int(centroid[1]), int(centroid[0])]
-
-# Affichage du resultat final
-utils.displayImage(imgContour, 'fin')
+imgFinale = kMean.ajouterIndicationCouleursZone(imgContour, imgNettoyee, rayonDisqueCouleur)
+utils.displayImage(imgFinale, 'Image finale')

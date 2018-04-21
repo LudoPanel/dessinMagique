6614ded1df6cedb7961b2a6a980c171239bb9626
Unknown <ludovic.panel@gmail.com>
2018-04-22 00:29:16 +0200

    rename

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cv2, kMean, utils
+import cv2, dessinMagique, utils
 
 #######
 ## Parametres a saisir par l'utilisateur
@@ -18,20 +18,20 @@ img = cv2.imread('Images/' + nomImage)
 utils.displayImage(img, 'Image initiale')
 
 # On applique kmean pour reduire le nombre de couleurs de l'image
-imgKMean = kMean.reducColor(img, nombreDeCouleur)
+imgKMean = dessinMagique.reducColor(img, nombreDeCouleur)
 utils.displayImage(imgKMean, 'Image kmean')
 
 # On nettoie le resultat obtenu (on enleve les zones trop petites)
-imgNettoyee = kMean.nettoyageImage(imgKMean, aireZoneMax, largeurZoneMax)
+imgNettoyee = dessinMagique.nettoyageImage(imgKMean, aireZoneMax, largeurZoneMax)
 utils.displayImage(imgNettoyee, 'Image nettoyee')
 
 # On extrait les contours de l'image
-imgContour = kMean.traceContour(img, imgNettoyee)
+imgContour = dessinMagique.traceContour(img, imgNettoyee)
 utils.displayImage(imgContour, 'Image contour')
 
 # On pose les indications dans les zones
 # imgFinale = kMean.ajouterIndicationCouleursZoneLabellisation(imgContour, imgNettoyee, rayonDisqueCouleur)
 # utils.displayImage(imgFinale, 'Image finale methode 1')
 
-imgFinale = kMean.ajouterIndicationCouleursZoneErosionSuccessives(imgContour, imgNettoyee, rayonDisqueCouleur)
+imgFinale = dessinMagique.ajouterIndicationCouleursZoneErosionSuccessives(imgContour, imgNettoyee, rayonDisqueCouleur)
 utils.displayImage(imgFinale, 'Image finale methode 2')
